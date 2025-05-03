875ce5a5ce29d6c72eacc8f3c83d957d0cb13814
Sechin Sergey Sergeevich <113019150+Dilunder@users.noreply.github.com>
2025-05-03 10:17:31 +0300

    saved

---
+++ src/test/java/org/example/criptobot/CriptoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.criptobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
