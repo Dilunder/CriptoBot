7b794f6c97de756b86a389ee3422b47b9cb7b72d
Sechin Sergey Sergeevich <113019150+Dilunder@users.noreply.github.com>
2025-05-03 11:31:32 +0300

    working example

---
+++ src/main/java/org/example/criptobot/CryptoWebSocketHandler.java
@@ -0,0 +1,23 @@
+package org.example.criptobot;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class CryptoWebSocketHandler extends TextWebSocketHandler {
+    private CryptoService cryptoService;
+
+    public CryptoWebSocketHandler(CryptoService cryptoService) {
+        this.cryptoService = cryptoService;
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String symbol = message.getPayload();
+        String price = cryptoService.getCryptoPrice(symbol);
+
+        session.sendMessage(new TextMessage(price));
+    }
+}
